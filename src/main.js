b37a2e5a7b24e82d61fb31363d93cec0124e3ed4
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2025-11-06 18:20:41 -0300

    Mudando para arquivos separados

---
+++ src/main.js
@@ -0,0 +1,9 @@
+//<!-- JavaScript para o scroll suave (melhora a UX) -->
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
